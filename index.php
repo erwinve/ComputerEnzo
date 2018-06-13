0bd692451a75265eedcd95fcea0a3d359cd3062d
erwinve <erwinvenneker13@gmail.com>
2018-06-13 23:17:27 +0200

    Added head, nav, main.css, and header

--- index.php
+++ index.php
@@ -1,4 +1,11 @@
 <?php 
-include('head.php');
-include('navbar.php');
+include('./Includes/head.php');
+include('./Includes/navbar.php');
 ?>
+    <header class="blurheader">
+        <div class="headerimage"></div>
+            <h1 class="display-1">ComputerEnzo</h1>
+            <p class="lead">jouw hulp bij computers, laptops, mobieltjes, hardware en software problemen</p>
+        </div>
+    </header>
+
