f9714ab5aabc815d77323b55cd86dbc5fbf7d4d5
erwinve <erwinvenneker13@gmail.com>
2018-06-14 17:28:59 +0200

    Added Overmij and Started Promotionsection

--- index.php
+++ index.php
@@ -2,10 +2,59 @@
 include('./Includes/head.php');
 include('./Includes/navbar.php');
 ?>
+    <!-- header -->
     <header class="blurheader">
         <div class="headerimage"></div>
-            <h1 class="display-1">ComputerEnzo</h1>
-            <p class="lead">jouw hulp bij computers, laptops, mobieltjes, hardware en software problemen</p>
+            <h1 class="display-1 padtop200">ComputerEnzo</h1>
+            <hr class="headerline">
+            <p class="lead">Jouw hulp bij computers, laptops, mobieltjes, hardware en software problemen.</p>
+            <a href="contact.php" class="btn btn-lg btn-primary">Contact</a>
+            <a href="boeken.php" class="btn btn-lg btn-primary">Afspraak maken</a>
         </div>
     </header>
+
+    <!-- promotionsection -->
+    <div class="container">
+        <div class="row">
+            <div class="col-3 text-center border-blue">
+            <h5>Blog</h5>
+            </div>
+            <div class="col-3 text-center border-blue">
+            <h5>Contact</h5>
+            </div>
+            <div class="col-3 text-center border-blue">
+            <h5>Afspraak</h5>
+            </div>
+            <div class="col-3 text-center border-blue">
+            <h5>Webshop</h5>
+            </div>
+            
+        </div>   
+
+    <!-- overmij -->
+
+            <h4 class="display-4 text-center padtop50">Over mij</h4>
+            <hr class="h2line">
+            <img class="float-left" src="https://imgur.com/imu73C1.jpg" alt="Profilepicture">
+        <div class="ovcontent">
+            <p class="text-left">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nam tempore voluptatem laboriosam quo cum quam sunt fuga quaerat impedit at, ipsum voluptas similique architecto illum. Vel incidunt vitae perspiciatis accusamus.</p><br>
+            <i class="fab fa-facebook-square fa-3x text-blue"></i>
+            <i class="fab fa-linkedin fa-3x text-blue"></i>
+            <i class="fas fa-envelope-square fa-3x text-blue"></i>
+            <h5 class="padtop20">HTML/CSS</h5>
+            <div class="progress">
+                 <div class="progress-bar" role="progressbar" style="width: 75%" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100">HTML/CSS</div>
+            </div>
+            <h5>PHP</h5>
+            <div class="progress">
+                <div class="progress-bar" role="progressbar" style="width: 75%" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
+            </div>
+            <h5>Windows</h5>
+            <div class="progress">
+                <div class="progress-bar" role="progressbar" style="width: 75%" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
+            </div>
+
+
+        </div>
+    </div>
 
